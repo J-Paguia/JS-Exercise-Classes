7ba537841db81477c9520fe11c464d9c4d347aed
futurewave27 <57016552+futurewave27@users.noreply.github.com>
2020-01-30 16:09:55 -0800

    Partial Update

--- index.js
+++ index.js
@@ -41,8 +41,38 @@ class Airplane {
 */
 
 class Person {
+  constructor (name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
 
-}
+  eat(someFood){
+    if (this.stomach.length < 10) 
+      this.stomach.push(someFood)
+    }
+  
+  poop() {
+    this.stomach = [];
+  }
+
+  toString(){
+    return `${this.name} ${this.age}`;
+  }
+}  
+
+
+   // Person.prototype.eat = function(someFood) {
+    //   if (this.stomach.length < 10) return this.stomach.push(someFood);
+    // };
+    
+    // Person.prototype.poop = function() {
+    //   return (this.stomach = []);
+    // };
+    
+    // Person.prototype.toString = function() {
+    //   return `${this.name} ${this.age}`;
+    // };
 
 /*
   TASK 2
@@ -59,8 +89,37 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+  }
+
+  fill(gallons){
+    this.tank += gallons;
+  }
+
+  drive(distance, odometer){
+
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
+
 
 }
+
+
+
+// function Car(model, milesPerGallon) {
+//   this.model = model;
+//   this.milesPerGallon = milesPerGallon;
+//   this.tank = 0;
+//   this.odometer = 0;
+// }
+
+// Car.prototype.fill = function(gallons) {
+//   this.tank = this.tank + gallons;
+// };
 
 /*
   TASK 3
@@ -75,6 +134,15 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
 
 }
 
@@ -92,8 +160,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor(attributes){
+    super(attributes){
+      this.specialty = attributes.specialty;
+      this.favLanguage = 'JavaScript';
+      this.catchPhrase = `Don't forget the homies`;
+    }
 
+  
+  }
+  
+  
+ 
 }
 
 /*
